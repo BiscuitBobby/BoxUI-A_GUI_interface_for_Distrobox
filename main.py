5ac33731a0e33dad1616a0d5f1f17184f5037271
BiscuitBobby <87699062+BiscuitBobby@users.noreply.github.com>
2023-03-19 10:05:49 +0530

    ui update

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import sys
-from PySide6 import QtWidgets, QtGui
+from PySide6 import QtWidgets, QtGui, QtCore
 from PySide6.QtCore import Qt
 from Data.FetchData import *
 
 
 class button(QtWidgets.QPushButton):
     def __init__(self, text):
-        super().__init__(text)
-        self.setFixedSize(250,250)
+        super().__init__()
+        self.setFixedSize(250, 250)
+        pixmap = QtGui.QPixmap(dists[text]["icon"])
         self.setStyleSheet("background-color: #999999; border-radius: 10px")
 
         # Connect the clicked signal to a slot (function)
         self.clicked.connect(lambda: print(text))
-
-
+        if pixmap:
+            self.setIcon(pixmap)
+            self.setIconSize(QtCore.QSize(250, 250))
+        else:
+            self.setText(text)
+class ScrollArea(QtWidgets.QScrollArea):
+    def __init__(self, layout):
+        super().__init__()
+        self.setWidgetResizable(True)
+        scroll_widget = QtWidgets.QWidget()
+        scroll_widget.setLayout(layout)
+        self.setWidget(scroll_widget)
+        #scroll_widget.setStyleSheet("background-color: #444654")
+        layout.setAlignment(Qt.AlignLeft | Qt.AlignTop)
 
 
 class MainWidget(QtWidgets.QWidget):
@@ -44,32 +57,37 @@ class MainWidget(QtWidgets.QWidget):
         button_list = (self.start, self.delete, self.open)
 
         # Set the layouts and add the labels to it
-        right_layout = QtWidgets.QVBoxLayout()
-        right_layout.addWidget(self.image_label)
-        right_layout.addWidget(self.info)
+        details_layout = QtWidgets.QVBoxLayout()
+        right_layout = QtWidgets.QWidget()
+        right_layout.setStyleSheet("background-color: #444654")
+        right_layout.setLayout(details_layout)
+
+        details_layout.addWidget(self.image_label)
+        details_layout.addWidget(self.info)
         for i in button_list:
-            right_layout.addWidget(i)
+            details_layout.addWidget(i)
+
         self.left_layout = QtWidgets.QGridLayout()
-        self.setMinimumSize(800, 600)
+        left_container = ScrollArea(self.left_layout)
+        self.setMinimumSize(1000, 600)
         self.setBaseSize(1025, 800)
 
         # Adding the layouts to main layout
-        main_layout.addStretch(1)
-        main_layout.addLayout(self.left_layout)
-        main_layout.addLayout(right_layout)
-        right_layout.addStretch(1)
+        main_layout.addWidget(left_container)
+        main_layout.addWidget(right_layout)
         self.left_layout.rowStretch(1)
 
         # Set alignment of main layout to top center
-        right_layout.setAlignment(Qt.AlignTop | Qt.AlignRight)
-        self.left_layout.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+        details_layout.setAlignment(Qt.AlignTop | Qt.AlignRight)
+        left_container.setAlignment(Qt.AlignLeft | Qt.AlignTop)
         self.resizeEvent = self.onResize
 
+        right_layout.setMaximumWidth(400)
 
     def onResize(self, event):
         lst = []
         lnum = 0
-        wspace = int((self.width()-440)/250)
+        wspace = int((self.width() - 440) / 250)
         r = 1
         c = 1
         while self.left_layout.count():
@@ -81,7 +99,7 @@ class MainWidget(QtWidgets.QWidget):
 
         for i in (dists):
             lst.append(button(i))
-            #button(i).clicked.connect(lambda: self.image_label.setPixmap(icons((i.split(':'))[0])).scaled(250, 250, Qt.KeepAspectRatio, Qt.SmoothTransformation))
+            # button(i).clicked.connect(lambda: self.image_label.setPixmap(icons((i.split(':'))[0])).scaled(250, 250, Qt.KeepAspectRatio, Qt.SmoothTransformation))
             self.left_layout.addWidget(lst[lnum], r, c)
             lnum += 1
             if c < wspace:
@@ -91,6 +109,7 @@ class MainWidget(QtWidgets.QWidget):
 
         print(self.width(), self.height())
         print('space=', wspace)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
